da43db6b73bec79442b5e9a0180b12c08a187ecd
fernan-castro <fernando.castro1@upr.edu>
2025-02-21 18:27:46 -0400

    input validation and error handling in sing up screen, issues 128 and 129

--- lib/src/screens/signup_screen.dart
+++ lib/src/screens/signup_screen.dart
@@ -3,24 +3,45 @@ import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:semester_project__uprm_pet_adoption/src/providers/auth_provider.dart';
 
-class SignUpScreen extends ConsumerWidget {
-  SignUpScreen({super.key});
-
-  // Controllers for handling text input
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController firstNameController = TextEditingController();
-  final TextEditingController lastNameController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-  final TextEditingController confirmPasswordController = TextEditingController();
+class SignUpScreen extends ConsumerStatefulWidget {
+  const SignUpScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends ConsumerState<SignUpScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  late final TextEditingController emailController;
+  late final TextEditingController firstNameController;
+  late final TextEditingController lastNameController;
+  late final TextEditingController passwordController;
+  late final TextEditingController confirmPasswordController;
+
+  @override
+  void initState() {
+    super.initState();
+    emailController = TextEditingController();
+    firstNameController = TextEditingController();
+    lastNameController = TextEditingController();
+    passwordController = TextEditingController();
+    confirmPasswordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    firstNameController.dispose();
+    lastNameController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-        //AppBar is a prebuilt widget in Flutter
-        appBar: AppBar(
-            toolbarHeight: -5,
-            backgroundColor: Color(0xFF82B0FF),
-        ),
       body: Stack(
         children: [
           // Background Image
@@ -30,116 +51,173 @@ class SignUpScreen extends ConsumerWidget {
               fit: BoxFit.cover,
             ),
           ),
+          // Sign-Up Form
           Center(
             child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // Sign Up Title
-                  const Padding(
-                    padding: EdgeInsets.only(bottom: 10),
-                    child: Text(
-                      'Sign Up',
-                      style: TextStyle(
-                        fontSize: 35,
-                        fontWeight: FontWeight.w900,
-                        fontFamily: 'Archivo',
-                        color: Colors.black,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(bottom: 10),
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          fontSize: 35,
+                          fontWeight: FontWeight.w900,
+                          fontFamily: 'Archivo',
+                          color: Colors.black,
+                        ),
                       ),
                     ),
-                  ),
-                  // Form Container
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.85,
-                    padding: const EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(50),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.2),
-                          blurRadius: 10,
-                          spreadRadius: 2,
-                          offset: const Offset(0, 5),
-                        ),
-                      ],
-                    ),
-                    child: Column(
-                      children: [
-                        // Email Text Field
-                        buildTextField(controller: emailController, label: "Email", icon: Icons.email, obscureText: false),
-                        const SizedBox(height: 20),
-                        // First and Last Name Fields
-                        Row(
-                          children: [
-                            Expanded(child: buildTextField(controller: firstNameController, label: "First Name", obscureText: false)),
-                            const SizedBox(width: 10),
-                            Expanded(child: buildTextField(controller: lastNameController, label: "Last Name", obscureText: false)),
-                          ],
-                        ),
-                        const SizedBox(height: 20),
-                        // Password and Confirm Password Fields
-                        buildTextField(controller: passwordController, label: "Password", icon: Icons.lock_outline, obscureText: true),
-                        const SizedBox(height: 20),
-                        buildTextField(controller: confirmPasswordController, label: "Confirm Password", icon: Icons.lock_outline, obscureText: true),
-                        const SizedBox(height: 25),
-                        // Create Account Button
-                        buildButton(
-                          text: "Create Account",
-                          onPressed: () async {
-                            String email = emailController.text;
-                            String firstName = firstNameController.text;
-                            String lastName = lastNameController.text;
-                            String password = passwordController.text;
-                            String confirmPassword = confirmPasswordController.text;
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.85,
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(50),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.2),
+                            blurRadius: 10,
+                            spreadRadius: 2,
+                            offset: const Offset(0, 5),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        children: [
+                          buildTextField(
+                            label: "Email",
+                            icon: Icons.email,
+                            obscureText: false,
+                            controller: emailController,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Please enter an email";
+                              }
 
-                            // Validate Password Matching
-                            if (password != confirmPassword) {
-                              ref.read(statusMessageProvider.notifier).state = "Passwords do not match";
-                              return;
-                            }
+                              final allowedDomains = ['@gmail.com', '@yahoo.com', '@upr.edu'];
 
-                            // Example user ID based on name
-                            String userId = "${firstName}_$lastName";
-                            await ref.read(authProvider.notifier).signUp(
-                              userId: userId,
-                              email: email,
-                              password: password
-                            );
-                          },
-                        ),
-                        const SizedBox(height: 15),
-                        // Status Message Display
-                        Consumer(
-                          builder: (context, ref, child) {
-                            final message = ref.watch(statusMessageProvider);
-                            return Padding(
-                              padding: const EdgeInsets.all(10),
-                              child: Text(
-                                message,
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black,
+                              if (!RegExp(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
+                                  .hasMatch(value)) {
+                                return "Invalid email format";
+                              }
+
+                              bool isValDomain = allowedDomains.any((domain) => value.endsWith(domain));
+                              if (!isValDomain) {
+                                return "Please use a valid email domain";
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 20),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: buildTextField(
+                                  label: "First Name",
+                                  obscureText: false,
+                                  controller: firstNameController,
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return "Enter first name";
+                                    }
+                                    if(!RegExp(r"^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$").hasMatch(value)) {
+                                      return "Invalid characters";
+                                    }
+                                    return null;
+                                  },
                                 ),
-                                textAlign: TextAlign.center,
                               ),
-                            );
-                          },
-                        ),
-                      ],
+                              const SizedBox(width: 10),
+                              Expanded(
+                                child: buildTextField(
+                                  label: "Last Name",
+                                  obscureText: false,
+                                  controller: lastNameController,
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return "Enter last name";
+                                    }
+                                    if(!RegExp(r"^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$").hasMatch(value)) {
+                                      return "Invalid characters";
+                                    }
+                                    return null;
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 20),
+                          buildTextField(
+                            label: "Password",
+                            icon: Icons.lock_outline,
+                            obscureText: true,
+                            controller: passwordController,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Enter a password";
+                              } 
+                              if (value.length < 6) {
+                                return "Password must be at least 6 characters";
+                              }
+                              if (!RegExp(r'^(?=.*[0-9])(?=.*[!@#\$%^&*(),.?":{}|<>]).{6,}$').hasMatch(value)) {
+                                return "Must include a number and a special character";
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 20),
+                          buildTextField(
+                            label: "Confirm Password",
+                            icon: Icons.lock_outline,
+                            obscureText: true,
+                            controller: confirmPasswordController,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Confirm your password";
+                              } else if (value != passwordController.text) {
+                                return "Passwords do not match";
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 25),
+                          buildButton(
+                            text: "Create Account",
+                            onPressed: () async {
+                              if (_formKey.currentState!.validate()) {  // Validate form before proceeding
+                                String email = emailController.text;
+                                String firstName = firstNameController.text;
+                                String lastName = lastNameController.text;
+                                String password = passwordController.text;
+
+                                // Example user ID based on name
+                                String userId = "${firstName}_$lastName";
+
+                                await ref.read(authProvider.notifier).signUp(
+                                  userId: userId,
+                                  email: email,
+                                  password: password,
+                                );
+                              }
+                            },
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 25),
-                  // Return Button
-                  buildButton(
-                    text: "Return",
-                    onPressed: () { 
-                      ref.read(statusMessageProvider.notifier).state = ""; // Clear message when leaving
-                      context.go('/auth');
-                    },
-                  ),
-                ],
+                    const SizedBox(height: 25),
+                    buildButton(
+                      text: "Return",
+                      onPressed: () {
+                        ref.read(statusMessageProvider.notifier).state = ""; // Clear message when leaving
+                        context.go('/auth');
+                      },
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -148,49 +226,66 @@ class SignUpScreen extends ConsumerWidget {
     );
   }
 
-  // Method for Creating Input Fields
   Widget buildTextField({
-    required TextEditingController controller,
     required String label,
     IconData? icon,
     required bool obscureText,
+    required TextEditingController controller,
+    String? Function(String?)? validator,
   }) {
-    return TextField(
-      controller: controller,
-      obscureText: obscureText,
-      decoration: InputDecoration(
-        enabledBorder: const UnderlineInputBorder(
-          borderSide: BorderSide(color: Colors.black, width: 3.0),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TextFormField(
+          controller: controller,
+          obscureText: obscureText,
+          decoration: InputDecoration(
+            enabledBorder: const UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.black, width: 3.0),
+            ),
+            focusedBorder: const UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.black, width: 2.0),
+            ),
+            labelText: label,
+            labelStyle: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Archivo',
+              color: Colors.black,
+            ),
+            suffixIcon: icon != null ? Icon(icon, color: Color(0xFF5D5793)) : null,
+          ),
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Archivo',
+            color: Colors.black,
+          ),
+          validator: validator,
         ),
-        focusedBorder: const UnderlineInputBorder(
-          borderSide: BorderSide(color: Colors.black, width: 2.0),
-        ),
-        labelText: label,
-        labelStyle: const TextStyle(
-          fontWeight: FontWeight.bold,
-          fontFamily: 'Archivo',
-          color: Colors.black,
-        ),
-        suffixIcon: icon != null ? Icon(icon, color: Color(0xFF5D5793)) : null,
-      ),
-      style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold, fontFamily: 'Archivo', color: Colors.black),
+        const SizedBox(height: 10),
+      ],
     );
   }
 
-  // Method for Creating Buttons
   Widget buildButton({required String text, required VoidCallback onPressed}) {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
-        backgroundColor: const Color(0xFFC8E8FF), // Light blue background
-        foregroundColor: Colors.black, // Black text
-        minimumSize: const Size(200, 60), // Button size
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        backgroundColor: const Color(0xFFC8E8FF),
+        foregroundColor: Colors.black,
+        minimumSize: const Size(200, 60),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
       ),
       onPressed: onPressed,
-      child: Text(text, style: const TextStyle(fontFamily: 'Archivo', fontSize: 20, fontWeight: FontWeight.bold)),
+      child: Text(
+        text,
+        style: const TextStyle(
+          fontFamily: 'Archivo',
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
     );
   }
 }
-
-
-
