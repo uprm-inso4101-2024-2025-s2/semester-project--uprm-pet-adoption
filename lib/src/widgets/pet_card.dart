985f2e05002dce3cb8d4bed2d568e5fc1c3aa101
nataliavera6 <natalia.vera6@upr.edu>
2025-03-18 11:47:52 -0400

    user swipe shows new pet

--- lib/src/widgets/pet_card.dart
+++ lib/src/widgets/pet_card.dart
@@ -101,8 +101,10 @@ class _PetCardState extends State<PetCard> with SingleTickerProviderStateMixin {
     setState(() {
       _dragOffset = Offset.zero;
       _opacity = 1.0;
+      
     });
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -244,148 +246,6 @@ class _PetCardState extends State<PetCard> with SingleTickerProviderStateMixin {
       ),
     );
   }
-  //  @override
-  // Widget build(BuildContext context) {
-  //   return GestureDetector(
-  //     onHorizontalDragUpdate: _onHorizontalDragUpdate,
-  //     onHorizontalDragEnd: _onHorizontalDragEnd,
-  //     child: ClipRect(
-      
-  //     child: AnimatedOpacity(
-  //       duration: Duration(milliseconds: 200),
-  //       opacity: _opacity,
-  //       child: Transform.translate(
-  //         offset: _dragOffset,
-  //         child: Card(
-  //           color: Colors.yellow[100],
-  //           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-  //           elevation: 5,
-  //           child: Padding(
-  //             padding: const EdgeInsets.all(8.0),
-  //             child: Column(
-  //               children: [
-  //                 Stack(
-  //                   children: [
-  //                     // Image Slider
-  //                     ClipRRect(
-  //                       borderRadius: BorderRadius.circular(15),
-  //                       child: SizedBox(
-  //                         height: 250,
-  //                         child: PageView.builder(
-  //                           controller: _pageController,
-  //                           itemCount: widget.petImages.length,
-  //                           onPageChanged: (index) {
-  //                             setState(() => _currentPage = index);
-  //                           },
-  //                           itemBuilder: (context, index) {
-  //                             return Image.asset(
-  //                               widget.petImages[index],
-  //                               width: double.infinity,
-  //                               fit: BoxFit.cover,
-  //                             );
-  //                           },
-  //                         ),
-  //                       ),
-  //                     ),
 
-  //                     // Favorite Button (Top Right)
-  //                     Positioned(
-  //                       top: 10,
-  //                       right: 10,
-  //                       child: GestureDetector(
-  //                         onTap: _toggleFavorite,
-  //                         child: Icon(
-  //                           _isFavorite ? Icons.favorite : Icons.favorite_border,
-  //                           color: _isFavorite ? Colors.pinkAccent : Colors.pinkAccent,
-  //                           size: 30,
-  //                         ),
-  //                       ),
-  //                     ),
-
-  //                     // Pet Tags
-  //                     Positioned(
-  //                       top: 15,
-  //                       left: -10,
-  //                       child: Column(
-  //                         crossAxisAlignment: CrossAxisAlignment.start,
-  //                         children: widget.petTags.map((tag) {
-  //                           return Container(
-  //                             margin: EdgeInsets.only(bottom: 5),
-  //                             padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-  //                             decoration: BoxDecoration(
-  //                               color: Colors.white,
-  //                               borderRadius: BorderRadius.circular(5),
-  //                               boxShadow: [
-  //                                 BoxShadow(
-  //                                   color: Colors.black26,
-  //                                   blurRadius: 3,
-  //                                   offset: Offset(2, 2),
-  //                                 ),
-  //                               ],
-  //                             ),
-  //                             child: Text(
-  //                               tag,
-  //                               style: TextStyle(
-  //                                 fontWeight: FontWeight.bold,
-  //                                 fontSize: 12,
-  //                               ),
-  //                             ),
-  //                           );
-  //                         }).toList(),
-  //                       ),
-  //                     ),
-
-  //                     // Pet Description Overlay
-  //                     Positioned(
-  //                       bottom: 0,
-  //                       left: 0,
-  //                       right: 0,
-  //                       child: Container(
-  //                         padding: EdgeInsets.all(10),
-  //                         decoration: BoxDecoration(
-  //                           color: Colors.black.withOpacity(0.6),
-  //                           borderRadius: BorderRadius.only(
-  //                             bottomLeft: Radius.circular(15),
-  //                             bottomRight: Radius.circular(15),
-  //                           ),
-  //                         ),
-  //                         child: Text(
-  //                           widget.petDescription,
-  //                           style: TextStyle(
-  //                             color: Colors.white,
-  //                             fontWeight: FontWeight.bold,
-  //                             fontSize: 14,
-  //                           ),
-  //                           textAlign: TextAlign.center,
-  //                         ),
-  //                       ),
-  //                     ),
-  //                   ],
-  //                 ),
-
-  //                 // Page Indicator
-  //                 Row(
-  //                   mainAxisAlignment: MainAxisAlignment.center,
-  //                   children: List.generate(widget.petImages.length, (index) {
-  //                     return Container(
-  //                       margin: EdgeInsets.symmetric(horizontal: 3, vertical: 5),
-  //                       width: 8,
-  //                       height: 8,
-  //                       decoration: BoxDecoration(
-  //                         shape: BoxShape.circle,
-  //                         color: _currentPage == index ? Colors.blue : Colors.grey,
-  //                       ),
-  //                     );
-  //                   }),
-  //                 ),
-  //               ],
-  //             ),
-  //           ),
-  //         ),
-  //       ),
-  //     ),
-  //   ),
-  //   );
-  // }
 
 }
