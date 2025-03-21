e19a238c1d81f2ea5973c2f4c9a759979fcfaa4e
TaimaraColon <taimara.colon@upr.edu>
2025-03-21 19:40:44 -0400

    Implemented Chat Screen Header

--- lib/src/routes/app_router.dart
+++ lib/src/routes/app_router.dart
@@ -16,6 +16,8 @@ import '../screens/favorites_screen.dart';
 import '../screens/user_profile_screen.dart';
 import '../screens/map_screen.dart';
 import '../screens/search_screen.dart';
+import '../screens/settings_screen.dart';
+
 //This file contains the routes for all screens. It also manages transitions between screens.
 
 final GoRouter appRouter = GoRouter(
@@ -23,11 +25,11 @@ final GoRouter appRouter = GoRouter(
     final container = ProviderScope.containerOf(context);
     final isLoggedIn = container.read(authProvider);
 
-    if (!isLoggedIn && state.matchedLocation == '/' ||
-        !isLoggedIn && state.matchedLocation == 'menu') {
-      return '/auth'; // get unauthenticated users back to start screen (authentication screen)
-    }
-    return null;
+    // if (!isLoggedIn && state.matchedLocation == '/' ||
+    //     !isLoggedIn && state.matchedLocation == 'menu') {
+    //   return '/auth'; // get unauthenticated users back to start screen (authentication screen)
+    // }
+    // return null;
   },
   routes: [
     //Route for home screen
@@ -262,6 +264,23 @@ final GoRouter appRouter = GoRouter(
         },
       ),
     ),
+    // Route for chat settings
+    GoRoute(
+    path: '/settings',
+    pageBuilder: (context, state) => CustomTransitionPage(
+      child: const SettingsScreen(), // Replace with your actual settings screen widget
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        return SlideTransition(
+          position: Tween<Offset>(
+            begin: const Offset(1, 0),
+            end: Offset.zero,
+          ).animate(animation),
+          child: child,
+        );
+      },
+    ),
+  ),
+
 
   ],
 );
