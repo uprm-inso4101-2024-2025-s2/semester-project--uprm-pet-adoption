e63caf474087fa88fe4c0f33f643f769d222bacb
amarrero7 <alejandromarrero89@gmail.com>
2025-03-21 22:20:30 -0400

    fcm implemantation

---
+++ lib/services/firebase_cloud_messaging.dart
@@ -0,0 +1,65 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+class MessagingNotifications {
+  MessagingNotifications._();
+  static final MessagingNotifications instance = MessagingNotifications._();
+
+  Future<void> initialize() async {
+    // get permission from user to allow notifications
+    await getPermission();
+
+    // subscribe user to receive general notifications
+    await FirebaseMessaging.instance.subscribeToTopic("general");
+
+    // handles notifications when app is in the foreground, backgorund and terminated
+    await setupNotifications();
+  }
+
+  Future<void> getPermission() async {
+    await FirebaseMessaging.instance.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+  }
+
+  Future<void> setupNotifications() async {
+    const AndroidInitializationSettings androidSettings = AndroidInitializationSettings('@mipmap/ic_launcher');
+  
+    const InitializationSettings settings = InitializationSettings(
+      android: androidSettings,
+    );
+
+    await flutterLocalNotificationsPlugin.initialize(settings);
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          const NotificationDetails(
+            android: AndroidNotificationDetails(
+              'high_importance_channel',
+              'High Importance Notifications',
+              importance: Importance.max,
+              priority: Priority.high,
+            ),
+          ),
+        );
+      }
+    });
+  }
+}
