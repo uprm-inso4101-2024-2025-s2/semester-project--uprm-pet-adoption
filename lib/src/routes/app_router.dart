87bfca7a7d2b027c707645b61468b263a28a7061
yamilette-alemany <yamilette.alemany@upr.edu>
2025-03-22 23:06:01 -0400

    fixed menu integration with FAQ screen

--- lib/src/routes/app_router.dart
+++ lib/src/routes/app_router.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:semester_project__uprm_pet_adoption/src/screens/FAQs_screen.dart' hide FAQScreen;
+import 'package:semester_project__uprm_pet_adoption/src/screens/Settings_Screen.dart';
 import 'package:semester_project__uprm_pet_adoption/src/screens/chat_screen.dart';
 import 'package:semester_project__uprm_pet_adoption/src/screens/dms_screen.dart';
 import '../screens/home_screen.dart';
@@ -10,6 +12,10 @@ import '../screens/auth_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:semester_project__uprm_pet_adoption/src/providers/auth_provider.dart';
 import '../screens/gettoknow_screen.dart';
+import '../screens/AboutUs_screen.dart';
+import '../screens/Settings_Screen.dart';
+import '../screens/FAQ_screen.dart';
+
 import '../screens/match_making_screen.dart';
 import '../screens/pet_tips_screen.dart';
 import '../screens/favorites_screen.dart';
@@ -19,12 +25,9 @@ import '../screens/search_screen.dart';
 import 'package:semester_project__uprm_pet_adoption/src/screens/forgot_password_screen.dart';
 import 'package:semester_project__uprm_pet_adoption/src/screens/forgot_password_verification_screen.dart';
 import 'package:semester_project__uprm_pet_adoption/src/screens/forgot_password_change_screen.dart';
-import '../screens/settings_screen.dart';
-import 'package:semester_project__uprm_pet_adoption/src/screens/faq_screen.dart';
+//import '../screens/settings_screen.dart';
+//import 'package:semester_project__uprm_pet_adoption/src/screens/faq_screen.dart';
 
-import '../screens/AboutUs_screen.dart';
-import '../screens/Settings_Screen.dart';
-import '../screens/FAQs_screen.dart';
 
 //This file contains the routes for all screens. It also manages transitions between screens.
 
@@ -116,7 +119,7 @@ final GoRouter appRouter = GoRouter(
       ),
     ),
 
-// Route for forgot password screen
+// Route for forgot password screen 
 GoRoute(
   path: '/forgot_password',
   pageBuilder: (context, state) => CustomTransitionPage(
@@ -189,7 +192,7 @@ GoRoute(
   GoRoute(
     path: '/faq',
     pageBuilder: (context, state) => CustomTransitionPage(
-      child: const FAQScreen(),
+      child: const FAQ_Screen(),
       transitionsBuilder: (context, animation, secondaryAnimation, child) {
         return SlideTransition(
           position: Tween<Offset>(
@@ -218,26 +221,42 @@ GoRoute(
       ),
     ),
 
-    GoRoute(
-      path: '/chat',
-      pageBuilder: (context, state) => CustomTransitionPage(
-        child: const ChatScreen(),
-        transitionsBuilder: (context, animation, secondaryAnimation, child) {
-          return SlideTransition(
-            position: Tween<Offset>(
-              begin: const Offset(1, 0),
-              end: Offset.zero,
-            ).animate(animation),
-            child: child,
-          );
-        },
-      ),
-    ),
+    // GoRoute(
+    //   path: '/chat',
+    //   pageBuilder: (context, state) => CustomTransitionPage(
+    //     // child: const ChatScreen(),
+    //     transitionsBuilder: (context, animation, secondaryAnimation, child) {
+    //       return SlideTransition(
+    //         position: Tween<Offset>(
+    //           begin: const Offset(1, 0),
+    //           end: Offset.zero,
+    //         ).animate(animation),
+    //         child: child,
+    //       );
+    //     },
+    //   ),
+    // ),
+
+    // GoRoute(
+    //   path: '/dms',
+    //   pageBuilder: (context, state) => CustomTransitionPage(
+    //     child: const DMScreen(),
+    //     transitionsBuilder: (context, animation, secondaryAnimation, child) {
+    //       return SlideTransition(
+    //         position: Tween<Offset>(
+    //           begin: const Offset(1, 0),
+    //           end: Offset.zero,
+    //         ).animate(animation),
+    //         child: child,
+    //       );
+    //     },
+    //   ),
+    // ),
 
     GoRoute(
-      path: '/dms',
+      path: '/about_us',
       pageBuilder: (context, state) => CustomTransitionPage(
-        child: const DMScreen(),
+        child: const AboutUsScreen(),
         transitionsBuilder: (context, animation, secondaryAnimation, child) {
           return SlideTransition(
             position: Tween<Offset>(
@@ -281,21 +300,21 @@ GoRoute(
         },
       ),
     ),
-    GoRoute(
-      path: '/map',
-      pageBuilder: (context, state) => CustomTransitionPage(
-        child: const Maps(),
-        transitionsBuilder: (context, animation, secondaryAnimation, child) {
-          return SlideTransition(
-            position: Tween<Offset>(
-              begin: const Offset(1, 0),
-              end: Offset.zero,
-            ).animate(animation),
-            child: child,
-          );
-        },
-      ),
-    ),
+    // GoRoute(
+    //   path: '/map',
+    //   pageBuilder: (context, state) => CustomTransitionPage(
+    //     child: const Maps(),
+    //     transitionsBuilder: (context, animation, secondaryAnimation, child) {
+    //       return SlideTransition(
+    //         position: Tween<Offset>(
+    //           begin: const Offset(1, 0),
+    //           end: Offset.zero,
+    //         ).animate(animation),
+    //         child: child,
+    //       );
+    //     },
+    //   ),
+    // ),
     GoRoute(
       path: '/tips',
       pageBuilder: (context, state) => CustomTransitionPage(
@@ -359,51 +378,5 @@ GoRoute(
   ),
 
 
-
-    GoRoute(
-      path: '/about_us',
-      pageBuilder: (context, state) => CustomTransitionPage(
-        child: const DMScreen(),
-        transitionsBuilder: (context, animation, secondaryAnimation, child) {
-          return SlideTransition(
-            position: Tween<Offset>(
-              begin: const Offset(1, 0),
-              end: Offset.zero,
-            ).animate(animation),
-            child: child,
-          );
-        },
-      ),
-    ),
-    GoRoute(
-      path: '/settings',
-      pageBuilder: (context, state) => CustomTransitionPage(
-        child: const SettingsScreen(),
-        transitionsBuilder: (context, animation, secondaryAnimation, child) {
-          return SlideTransition(
-            position: Tween<Offset>(
-              begin: const Offset(1, 0),
-              end: Offset.zero,
-            ).animate(animation),
-            child: child,
-          );
-        },
-      ),
-    ),
-    GoRoute(
-      path: '/FAQs',
-      pageBuilder: (context, state) => CustomTransitionPage(
-        child: const FAQScreen(),
-        transitionsBuilder: (context, animation, secondaryAnimation, child) {
-          return SlideTransition(
-            position: Tween<Offset>(
-              begin: const Offset(1, 0),
-              end: Offset.zero,
-            ).animate(animation),
-            child: child,
-          );
-        },
-      ),
-    ),
   ],
 );
